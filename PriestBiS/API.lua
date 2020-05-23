09c7a40bee74bdcb1c86d872d1552249e5ec0a36
Emil Palm <7381434+c11epm@users.noreply.github.com>
2020-05-23 23:25:07 +0200

    Added Wrist, Hands, Feet, Waist, Legs, Rings and Trinkets

--- PriestBiS/API.lua
+++ PriestBiS/API.lua
@@ -2,7 +2,9 @@ local function updateTooltip(tooltip)
     name, _ = tooltip:GetItem()
     itemText = items[name]
     if itemText ~= nil then
-		tooltip:AddDoubleLine("BiS ranking:",itemText)
+        tooltip:AddDoubleLine("BiS ranking:",itemText)
+    else
+        tooltip:AddDoubleLine("BiS ranking:", "Ranking < top 10")
 	end
 
 end
