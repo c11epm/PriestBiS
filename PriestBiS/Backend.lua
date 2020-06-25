938608d45640d940704c88342ea54cdaeea5f3f5
Emil Palm <7381434+c11epm@users.noreply.github.com>
2020-06-25 21:56:44 +0200

    Tried to implement some automatic calculation of score field. no success yet

--- PriestBiS/Backend.lua
+++ PriestBiS/Backend.lua
@@ -1,11 +1,19 @@
 local function updateBiSTooltip(tooltip)
-    name, _ = tooltip:GetItem()
+    name, link = tooltip:GetItem()
     itemText = items[name]
     if itemText ~= nil then
         tooltip:AddDoubleLine("BiS ranking:",itemText)
     else
-        tooltip:AddDoubleLine("BiS ranking:", "Ranking < top 10")
+        tooltip:Add("HELLO")
+        stats = GetItemStats(link)
+        tooltip:Add(stats)
+        -- tooltip:AddDoubleLine("Test calc:", tostring(GetScoreForStats(stats)))
+        -- tooltip:AddDoubleLine("BiS ranking:", "Ranking < top 10")
 	end
+end
+
+local function GetScoreForStats(stats) 
+    --return stats["ITEM_MOD_STAMINA_SHORT"]
 end
 
 enabled = true
